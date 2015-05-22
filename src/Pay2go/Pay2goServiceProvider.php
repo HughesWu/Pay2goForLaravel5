619daf9c899c79ff1a6d1262b4037e9845bface0
unknown <hughes.wu@hughes-pc.pay2go.local>
2015-05-22 18:03:05 +0800

    fix

--- src/Pay2go/Pay2goServiceProvider.php
+++ src/Pay2go/Pay2goServiceProvider.php
@@ -8,8 +8,11 @@ class Pay2goServiceProvider extends ServiceProvider {
 
 	public function register()
     {
-		$pay2go = new Pay2goPaymentClass();
-		return $pay2go;
+		$this->app['pay2go'] = $this->app->share(function($app)
+		{
+			$pay2go = new Pay2goPaymentClass();
+			return $pay2go;
+		});
     }
 
 }
